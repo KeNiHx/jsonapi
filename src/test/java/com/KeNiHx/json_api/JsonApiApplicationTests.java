680208faaa3e1317791c732ebaf53924e863712b
KeNiHx <klam291@mtroyal.ca>
2020-11-05 10:20:04 -0700

    first commit

---
+++ src/test/java/com/KeNiHx/json_api/JsonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KeNiHx.json_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
